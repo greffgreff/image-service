e5bed756ea1d5ce7cd48f1d9464e6d5cd3b011d5
chandle greff <unconfigured@null.spigotmc.org>
2022-04-15 16:14:27 +0200

    Added boiler classes

--- src/main/java/io/rently/imageservice/ImageAPI.java
+++ src/main/java/io/rently/imageservice/ImageAPI.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ImageserviceApplication {
+public class ImageAPI {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ImageserviceApplication.class, args);
+		SpringApplication.run(ImageAPI.class, args);
 	}
 
 }

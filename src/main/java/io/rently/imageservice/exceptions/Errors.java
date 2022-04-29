af2b1ce258e1d854cd4f845cd9d44f7cd1e5edb2
chandle greff <unconfigured@null.spigotmc.org>
2022-04-29 17:37:30 +0200

    Added middleware

--- src/main/java/io/rently/imageservice/exceptions/Errors.java
+++ src/main/java/io/rently/imageservice/exceptions/Errors.java
@@ -4,5 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.server.ResponseStatusException;
 
 public class Errors {
-    public static final ResponseStatusException NO_IMAGE = new ResponseStatusException(HttpStatus.NOT_FOUND, "Image not found");;
+    public static final ResponseStatusException NO_IMAGE = new ResponseStatusException(HttpStatus.NOT_FOUND, "Image not found");
+    public static final ResponseStatusException INVALID_REQUEST = new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid request made");
+    public static final ResponseStatusException UNAUTHORIZED_REQUEST = new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Request is either no longer valid or has been tampered with");
 }

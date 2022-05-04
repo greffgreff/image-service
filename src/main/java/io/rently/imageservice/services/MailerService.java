74485f1ad7c2f27898c1df715818cb7ae8514078
chandle greff <unconfigured@null.spigotmc.org>
2022-05-04 04:19:39 +0200

    Added JWT to mailer service

--- src/main/java/io/rently/imageservice/services/MailerService.java
+++ src/main/java/io/rently/imageservice/services/MailerService.java
@@ -1,6 +1,10 @@
 package io.rently.imageservice.services;
 
 import io.rently.imageservice.utils.Broadcaster;
+import io.rently.imageservice.utils.Jwt;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
@@ -11,8 +15,13 @@ import java.util.Map;
 
 @Component
 public class MailerService {
-    public static final String BASE_URL = "http://localhost:8084/api/v1/emails/dispatch/";
+    public static String BASE_URL;
     private static final RestTemplate restTemplate = new RestTemplate();
+
+    @Value("${mailer.baseurl}")
+    public void setBaseUrl(String baseUrl) {
+        MailerService.BASE_URL = baseUrl;
+    }
 
     public static void dispatchErrorToDevs(Exception exception) {
         Broadcaster.info("Dispatching error report...");
@@ -24,8 +33,11 @@ public class MailerService {
         report.put("cause", exception.getCause());
         report.put("trace", Arrays.toString(exception.getStackTrace()));
         report.put("exceptionType", exception.getClass());
+        HttpHeaders headers = new HttpHeaders();
+        headers.setBearerAuth(Jwt.generateBearerToken());
+        HttpEntity<Map<String, Object>> body = new HttpEntity<>(report, headers);
         try {
-            restTemplate.postForObject(BASE_URL, report, String.class);
+            restTemplate.postForObject(BASE_URL + "api/v1/emails/dispatch/", body, String.class);
         } catch (Exception ex) {
             Broadcaster.warn("Could not dispatch error report.");
             Broadcaster.error(ex);

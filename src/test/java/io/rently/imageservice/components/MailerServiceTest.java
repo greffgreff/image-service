7a99ee19f748e02eb4bf9f61e2617837e5cb3b88
chandle greff <unconfigured@null.spigotmc.org>
2022-06-02 14:43:40 +0200

    Added test classes

---
+++ src/test/java/io/rently/imageservice/components/MailerServiceTest.java
@@ -0,0 +1,12 @@
+package io.rently.imageservice.components;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MailerServiceTest {
+
+    @Test
+    void dispatchErrorToDevs() {
+    }
+}
